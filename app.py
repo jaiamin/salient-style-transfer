3bf5b501afad4afcf7d23969b9cabae5f1217b63
jaiamin <jaiamin130@gmail.com>
2024-09-25 00:22:43 -0400

    Reduce GPU duration

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ for style_name, style_img_path in style_options.items():
         style_features = model(style_img)
     cached_style_features[style_name] = style_features 
 
-@spaces.GPU(duration=15)
+@spaces.GPU(duration=10)
 def run(content_image, style_name, style_strength=5, progress=gr.Progress(track_tqdm=True)):
     yield None
     content_img, original_size = preprocess_img(content_image, img_size)
